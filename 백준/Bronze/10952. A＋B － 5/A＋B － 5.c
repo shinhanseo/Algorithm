0ce78bdb8bb1928d3716ab55ccc165d5264fdddd
신한서 <37097801+shinhanseo@users.noreply.github.com>
2025-04-07 11:36:05 +0900

    [Bronze V] Title: A+B - 5, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+  int cnt, num1, num2;
+  while (1)
+  {
+    scanf("%d %d", &num1, &num2);
+    if (num1 == 0 && num2 == 0)
+    {
+      break;
+    }
+
+    printf("%d\n", num1 + num2);
+  }
+}

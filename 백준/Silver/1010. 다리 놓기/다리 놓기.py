0a3f2529b116f4be82a1b1759c7cb91110ac2e79
신한서 <37097801+shinhanseo@users.noreply.github.com>
2025-04-07 11:53:45 +0900

    [Silver V] Title: 다리 놓기, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.py
@@ -0,0 +1,14 @@
+import sys
+
+def fac(N) :
+  result = 1
+  for i in range(1, N+1) :
+    result *= i
+
+  return result
+
+T = int(sys.stdin.readline())
+for _ in range(T) :
+  K, N = map(int, sys.stdin.readline().split())
+  print(fac(N) // (fac(N-K) * fac(K)))
+

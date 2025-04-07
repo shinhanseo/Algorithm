ef1fe624af3ba928c70ea1c294e8c55750d16ced
신한서 <37097801+shinhanseo@users.noreply.github.com>
2025-04-07 11:35:45 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int num;
+    scanf("%d", &num);
+    for(int i=0 ; i<num ; i++){
+        for(int j=0 ; j<i+1 ; j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+}

6bf7890244f905372b04a371602d7d18a6c89428
신한서 <37097801+shinhanseo@users.noreply.github.com>
2025-04-07 11:52:37 +0900

    [Silver IV] Title: 제로, Time: 68 ms, Memory: 33192 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.py
@@ -0,0 +1,13 @@
+import sys
+
+K = int(sys.stdin.readline())
+stack = []
+
+for _ in range(K) :
+  num = int(sys.stdin.readline())
+  if(num == 0) :
+    del stack[-1]
+  else :
+    stack.append(num)
+
+print(sum(stack))

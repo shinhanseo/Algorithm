edc8b34bc1ff068607de1ce93258ca32368b2a66
신한서 <37097801+shinhanseo@users.noreply.github.com>
2025-04-07 11:52:29 +0900

    [Silver IV] Title: 스택 2, Time: 796 ms, Memory: 88788 KB -BaekjoonHub

---
+++ 백준/Silver/28278. 스택 2/스택 2.py
@@ -0,0 +1,26 @@
+import sys
+
+N = int(sys.stdin.readline())
+stack = []
+
+for _ in range(N) :
+  order = sys.stdin.readline().split()
+  if(order[0] == '1') :
+    stack.append(order[1])
+  elif(order[0] == '2') :
+    if(len(stack) == 0) :
+      print('-1')
+    else :
+      print(stack.pop())
+  elif(order[0] == '3') :
+    print(len(stack))
+  elif(order[0] =='4') :
+    if(len(stack) == 0) :
+      print('1')
+    else :
+      print('0')
+  else :
+    if(len(stack) != 0) :
+      print(stack[-1])
+    else :
+      print('-1')

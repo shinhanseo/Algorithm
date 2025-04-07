3ffdbeb1efc8110d2dc826abd98068bcda5d04cc
신한서 <37097801+shinhanseo@users.noreply.github.com>
2025-04-07 11:39:50 +0900

    [Bronze II] Title: 상수, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2908. 상수/상수.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+  int num1, num2, re_num1, re_num2;
+  scanf("%d %d", &num1, &num2);
+
+  re_num1 = (num1 % 10) * 100 + ((num1 % 100) / 10) * 10 + (num1 / 100);
+  re_num2 = (num2 % 10) * 100 + ((num2 % 100) / 10) * 10 + (num2 / 100);
+
+  printf("%d", (re_num1 > re_num2) ? re_num1 : re_num2);
+}

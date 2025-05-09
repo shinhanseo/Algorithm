816b1026d252d899e592c102724238fb6d084e31
Hanseo <37097801+shinhanseo@users.noreply.github.com>
2025-05-09 16:51:32 +0900

    [level 2] Title: 오픈채팅방, Time: 144.80 ms, Memory: 53.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.py
@@ -0,0 +1,26 @@
+def solution(record):
+    dic = {}
+    answer = []
+    result = []
+    for i in record :
+        state = i[0] 
+
+        if state == 'E' :
+            state, user, name = i.split()
+            dic[user] = name
+            result.append([state,user])
+
+        elif state == 'L' :
+            state, user = i.split()
+            result.append([state,user])
+
+        else :
+            state, user, name = i.split()
+            dic[user] = name
+            
+    for state, user in result :
+        if(state == "Enter") :
+            answer.append(f"{dic[user]}님이 들어왔습니다.")
+        else :
+            answer.append(f"{dic[user]}님이 나갔습니다.")
+    return answer

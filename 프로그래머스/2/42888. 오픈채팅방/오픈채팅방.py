781e912fe595cb11a08d25694f74b71032971d17
Hanseo <37097801+shinhanseo@users.noreply.github.com>
2025-05-09 16:52:41 +0900

    [level 2] Title: 오픈채팅방, Time: 150.04 ms, Memory: 53.8 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.py
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.py
@@ -4,20 +4,16 @@ def solution(record):
     result = []
     for i in record :
         state = i[0] 
-
         if state == 'E' :
             state, user, name = i.split()
             dic[user] = name
             result.append([state,user])
-
         elif state == 'L' :
             state, user = i.split()
             result.append([state,user])
-
         else :
             state, user, name = i.split()
-            dic[user] = name
-            
+            dic[user] = name 
     for state, user in result :
         if(state == "Enter") :
             answer.append(f"{dic[user]}님이 들어왔습니다.")

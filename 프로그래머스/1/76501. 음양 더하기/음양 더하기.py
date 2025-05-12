07cb5f68e1d99adc0feff2b3f399e6b670a32a35
Hanseo <37097801+shinhanseo@users.noreply.github.com>
2025-05-12 16:44:12 +0900

    [level 1] Title: 음양 더하기, Time: 0.06 ms, Memory: 9.19 MB -BaekjoonHub

---
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.py
@@ -0,0 +1,9 @@
+def solution(absolutes, signs) :
+    sum = 0
+    for num, sign in zip(absolutes, signs) :
+        if sign :
+            sum += num
+        else :
+            sum -= num
+
+    return sum

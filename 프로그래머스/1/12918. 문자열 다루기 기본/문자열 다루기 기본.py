c0753993e6bdf206125aabc23b5147be254c77af
Hanseo <37097801+shinhanseo@users.noreply.github.com>
2025-05-13 10:40:24 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.00 ms, Memory: 9.14 MB -BaekjoonHub

---
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.py
@@ -0,0 +1,9 @@
+def solution(s):
+    num = ['0','1','2','3','4','5','6','7','8','9']
+    if len(s) == 4 or len(s) == 6 :
+        for i in s :
+            if i not in num :
+                return False
+        return True
+    else :
+        return False
